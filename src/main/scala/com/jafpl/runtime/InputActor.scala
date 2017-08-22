a4aead266e2bb611b26bbd1a6b8ac24fbfbd14eb
Norman Walsh <ndw@nwalsh.com>
2017-08-22 07:36:07 -0500

    Fix bug where writes would sometimes occur too early

--- src/main/scala/com/jafpl/runtime/InputActor.scala
+++ src/main/scala/com/jafpl/runtime/InputActor.scala
@@ -2,7 +2,7 @@ package com.jafpl.runtime
 
 import akka.actor.ActorRef
 import com.jafpl.graph.Node
-import com.jafpl.runtime.GraphMonitor.{GClose, GException, GFinished}
+import com.jafpl.runtime.GraphMonitor.{GClose, GException, GFinished, GOutput}
 import com.jafpl.steps.StepDataProvider
 
 private[runtime] class InputActor(private val monitor: ActorRef,
@@ -18,9 +18,13 @@ private[runtime] class InputActor(private val monitor: ActorRef,
 
   private def runIfReady(): Unit = {
     trace(s"RNIFR $node $readyToRun ${consumer.closed}", "StepExec")
-
     if (readyToRun && consumer.closed) {
-      run()
+      for (item <- consumer.items) {
+        monitor ! GOutput(node, "result", item)
+      }
+      consumer.clear()
+      monitor ! GClose(node, "result")
+      monitor ! GFinished(node)
     }
   }
 }
--- src/main/scala/com/jafpl/runtime/InputProxy.scala
+++ src/main/scala/com/jafpl/runtime/InputProxy.scala
@@ -3,17 +3,25 @@ package com.jafpl.runtime
 import akka.actor.ActorRef
 import com.jafpl.graph.Node
 import com.jafpl.runtime.GraphMonitor.{GClose, GOutput}
-import com.jafpl.steps.{StepDataProvider, DataProvider}
+import com.jafpl.steps.{DataProvider, StepDataProvider}
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 class InputProxy(private val monitor: ActorRef,
                  private val runtime: GraphRuntime,
                  private val node: Node) extends DataProvider {
   var _closed = false
+  val _items = mutable.ListBuffer.empty[Any]
 
   def closed: Boolean = _closed
+  private[runtime] def items: ListBuffer[Any] = _items
+  private[runtime] def clear() = {
+    _items.clear()
+  }
 
   def send(item: Any): Unit = {
-    monitor ! GOutput(node, "result", item)
+    _items += item
   }
 
   def close(): Unit = {
