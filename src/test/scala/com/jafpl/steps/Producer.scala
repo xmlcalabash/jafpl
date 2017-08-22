04926f697635d5d33fff41940e2fed98e5093d16
Norman Walsh <ndw@nwalsh.com>
2017-08-22 09:36:52 -0500

    Add a single string constructor

--- src/test/scala/com/jafpl/steps/Producer.scala
+++ src/test/scala/com/jafpl/steps/Producer.scala
@@ -1,6 +1,10 @@
 package com.jafpl.steps
 
 class Producer(val items: List[String]) extends DefaultStep {
+  def this(str: String) {
+    this(List(str))
+  }
+
   override def inputSpec = PortBindingSpecification.NONE
   override def outputSpec = PortBindingSpecification.RESULTSEQ
 
