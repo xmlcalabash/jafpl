8919d7107d09d52e6092168cea611ff6dd0c7f99
Norman Walsh <ndw@nwalsh.com>
2017-09-03 15:19:19 -0500

    Updated binding tests

--- src/test/scala/com/jafpl/test/BindingSpec.scala
+++ src/test/scala/com/jafpl/test/BindingSpec.scala
@@ -2,15 +2,65 @@ package com.jafpl.test
 
 import com.jafpl.graph.Graph
 import com.jafpl.io.BufferConsumer
+import com.jafpl.messages.Metadata
 import com.jafpl.primitive.PrimitiveRuntimeConfiguration
 import com.jafpl.runtime.GraphRuntime
-import com.jafpl.steps.{BufferSink, Count, ProduceBinding}
+import com.jafpl.steps.{BufferSink, Count, Identity, ProduceBinding}
 import org.scalatest.FlatSpec
 
 class BindingSpec extends FlatSpec {
   var runtimeConfig = new PrimitiveRuntimeConfiguration()
 
-  "A binding " should " be available" in {
+  "An external document binding " should " be consumable" in {
+    val graph = new Graph()
+
+    val pipeline = graph.addPipeline()
+    val ident    = pipeline.addAtomic(new Identity(), "ident")
+
+    graph.addInput(pipeline, "source")
+    graph.addEdge(pipeline, "source", ident, "source")
+    graph.addEdge(ident, "result", pipeline, "result")
+    graph.addOutput(pipeline, "result")
+
+    graph.close()
+    val runtime = new GraphRuntime(graph, runtimeConfig)
+
+    runtime.inputs("source").receive("foo", "Hello, World", Metadata.STRING)
+
+    val bc = new BufferConsumer()
+    runtime.outputs("result").setConsumer(bc)
+
+    runtime.run()
+
+    assert(bc.items.size == 1)
+    assert(bc.items.head == "Hello, World")
+  }
+
+  "An unbound external document binding " should " provides no documents" in {
+    val graph = new Graph()
+
+    val pipeline = graph.addPipeline()
+    val ident    = pipeline.addAtomic(new Identity(), "ident")
+
+    graph.addInput(pipeline, "source")
+    graph.addEdge(pipeline, "source", ident, "source")
+    graph.addEdge(ident, "result", pipeline, "result")
+    graph.addOutput(pipeline, "result")
+
+    graph.close()
+    val runtime = new GraphRuntime(graph, runtimeConfig)
+
+    //runtime.inputs("source").receive("foo", "Hello, World", Metadata.STRING)
+
+    val bc = new BufferConsumer()
+    runtime.outputs("result").setConsumer(bc)
+
+    runtime.run()
+
+    assert(bc.items.isEmpty)
+  }
+
+  "A variable binding " should " be available" in {
     var bc = new BufferSink()
 
     val graph = new Graph()
@@ -34,7 +84,7 @@ class BindingSpec extends FlatSpec {
     assert(bc.items.head == "twelve")
   }
 
-  "An external binding " should " be consumable" in {
+  "An external variable binding " should " be consumable" in {
     val graph = new Graph()
 
     val binding  = graph.addBinding("foo")
@@ -60,7 +110,7 @@ class BindingSpec extends FlatSpec {
     assert(bc.items.head == "Spoon!")
   }
 
-  "Reading an external binding twice " should " work" in {
+  "Reading an external variable binding twice " should " work" in {
     val graph = new Graph()
 
     val binding  = graph.addBinding("foo")
@@ -93,7 +143,7 @@ class BindingSpec extends FlatSpec {
     assert(bc.items.head == 2)
   }
 
-  "Leaving an external binding unbound " should " be an error" in {
+  "Leaving an external variable binding unbound " should " be an error" in {
     val graph = new Graph()
 
     val binding  = graph.addBinding("foo")
