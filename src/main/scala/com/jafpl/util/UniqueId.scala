b7f97d358ba6db794cf01ef3f1d209fb4fe03cfa
Norman Walsh <ndw@nwalsh.com>
2017-09-17 15:06:20 +0100

    Make synchronized for multi-threading

--- src/main/scala/com/jafpl/util/UniqueId.scala
+++ src/main/scala/com/jafpl/util/UniqueId.scala
@@ -3,8 +3,10 @@ package com.jafpl.util
 private[jafpl] object UniqueId {
   private var theNextId: Long = 0
   def nextId: Long = {
-    val id = theNextId
-    theNextId = theNextId + 1
-    id
+    this.synchronized {
+      val id = theNextId
+      theNextId = theNextId + 1
+      id
+    }
   }
 }
